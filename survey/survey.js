de3606486073e857808889725cd01ccb1b67a2d5
Zsigmond Czine <zsigmond.czine@everit.biz>
2016-10-17 16:54:31 +0200

    add survey script

---
+++ survey/survey.js
@@ -0,0 +1,11 @@
+function getSurveyStartDateString() {
+  return "2016-10-07";
+}
+
+function getSurveyStartDate() {
+  return new Date(getSurveyStartDateString());
+}
+
+function showSurvey(){
+	return false;
+}
